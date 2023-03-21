f7798ab1f2b810b133b5ad933b4902931f675b49
aaryan1122 <aaryanpunjabi2002@gmail.com>
2023-03-21 11:52:32 +0530

    second

--- bmi.js
+++ bmi.js
@@ -9,7 +9,7 @@ app.post("/index.html",function (req,res) {
     var h=Number(req.body.height)
     var w=Number(req.body.weight)
     var bmi= Math.round((w/(Math.pow(h,2))*100.0))/100.0
-    //21.34567 * 100 = 2134.567 => 2134 => 21.34
+    //21.34567 * 100 = 2134.567 => 2134 => 21.33
     res.send("Your BMI is: "+bmi)
 })
 app.listen(3000,function (req,res) {
